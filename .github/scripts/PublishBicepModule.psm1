6358a251fa18a25160c3f5a7cdf6b4fe42822b93
Sebastian Gredal <51902882+SebastianGredal@users.noreply.github.com>
2023-10-13 14:20:55 +0200

    fix spelling

--- .github/scripts/PublishBicepModule.psm1
+++ .github/scripts/PublishBicepModule.psm1
@@ -95,7 +95,7 @@ function Publish-ChangedModule {
       if ([System.Version]$latestVersion -ge [System.Version]$version) {
         Write-Error "The version in the $filename.bicep file is $version, and is therefore lower than or equal to the latest version, $latestVersion in the container registry"
       }
-      Write-Information -MessageData "The latest version for file: $filename is $version, and will set as the latest replacing the previous version $latestVersion"
+      Write-Information -MessageData "The latest version for file: $filename is $version, and will be set as the latest version replacing the previous version $latestVersion"
     }
 
     $modulePath = $parentFolder + '/' + $filename + ':' + $version
