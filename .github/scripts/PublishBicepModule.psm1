ac2d1be801e2473713025fa54bf406411fd391fa
Sebastian Gredal <51902882+SebastianGredal@users.noreply.github.com>
2023-10-13 12:57:59 +0200

    remove erroraction stop

--- .github/scripts/PublishBicepModule.psm1
+++ .github/scripts/PublishBicepModule.psm1
@@ -21,6 +21,32 @@ function Get-ChangedModule {
   return $changedBicepFiles
 }
 
+<#
+.SYNOPSIS
+Publishes changed Bicep modules to an Azure Container Registry.
+
+.DESCRIPTION
+This function publishes changed Bicep modules to an Azure Container Registry (ACR). It first builds the Bicep file, gets the version from the .json file, and checks if the module already exists in the ACR. If the module exists, it compares the latest version in the ACR to the version in the .bicep file. If the version in the .bicep file is lower than or equal to the latest version in the ACR, it throws an error. If the module does not exist in the ACR, it publishes the .bicep file to the ACR with the semver tag.
+
+.PARAMETER fromCommit
+The commit hash or branch name to compare changes from. If not specified, it will compare changes from the last commit.
+
+.PARAMETER toCommit
+The commit hash or branch name to compare changes to. If not specified, it will compare changes to the current working directory.
+
+.PARAMETER registryLoginServer
+The login server for the Azure Container Registry.
+
+.PARAMETER documentationUri
+The URI for the documentation of the Bicep module.
+
+.EXAMPLE
+Publish-ChangedModule -registryLoginServer "myregistry.azurecr.io" -documentationUri "https://myregistry.azurecr.io/docs" -Verbose
+Publishes all changed Bicep modules to the specified Azure Container Registry.
+
+.NOTES
+This function requires the Azure PowerShell module and the Bicep CLI to be installed.
+#>
 function Publish-ChangedModule {
   [CmdletBinding(SupportsShouldProcess = $true)]
   param(
@@ -52,7 +78,7 @@ function Publish-ChangedModule {
 
     # Check if the module exists in the ACR
     try {
-      $existingTags = Get-AzContainerRegistryTag -RegistryName $registryName -Repository "$parentFolder/$filename" -ErrorAction Stop
+      $existingTags = Get-AzContainerRegistryTag -RegistryName $registryName -Repository "$parentFolder/$filename"
     }
     catch {
       if ($_.Exception.Message -like '*not found*') {
@@ -66,7 +92,7 @@ function Publish-ChangedModule {
       # If the module exists, get the latest version and compare it to the version in the .bicep file
       $latestVersion = ($existingTags).Tags.Name | Sort-Object | Select-Object -Last 1
       if ([System.Version]$latestVersion -ge [System.Version]$version) {
-        Write-Error "The version in the $filename.bicep file is $version, and is therefore lower than or equal to the latest version, $latestVersion in the container registry" -ErrorAction Stop
+        Write-Error "The version in the $filename.bicep file is $version, and is therefore lower than or equal to the latest version, $latestVersion in the container registry"
       }
     }
 
@@ -75,7 +101,7 @@ function Publish-ChangedModule {
     # Publish the .bicep file to the ACR with the semver tag
     if ($PSCmdlet.ShouldProcess("$file", "Publish to ACR with tag $version")) {
       try {
-        Publish-AzBicepModule -FilePath $file -Target $target -DocumentationUri $documentationUri -ErrorAction Stop
+        Publish-AzBicepModule -FilePath $file -Target $target -DocumentationUri $documentationUri
       }
       catch {
         if ($_.Exception.Message) {
